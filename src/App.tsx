b6904cb495a8389e420dd616e7453b38c54b921f
daz <frozendevs@hotmail.com>
2025-04-08 15:56:30 +0400

    init

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React, { FC, useEffect, useState } from "react";
+
+import "./App.css";
+
+const App: FC = () => {
+  const [searchString, setSearchString] = useState<string>("");
+
+  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchString(e.target.value);
+  };
+
+  useEffect(() => {
+    if (searchString !== "") {
+      const timeoutId = setTimeout(() => console.log(searchString), 500);
+      return () => clearTimeout(timeoutId);
+    }
+  }, [searchString]);
+
+  return (
+    <>
+      <div>
+        <input value={searchString} onChange={onInputChange} />
+      </div>
+      <div>{searchString}</div>
+    </>
+  );
+};
+
+export default App;
